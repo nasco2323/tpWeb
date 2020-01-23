f43be0132c3ec4e4c6e62aca0af89a276398d035
narcissekouadio <kouadio.yao.narcisse2@gmail.com>
2020-01-23 11:48:44 +0100

    class du modèle

--- model.js
+++ model.js
@@ -1,3 +1,32 @@
 
 // Implémenter ici les 4 classes du modèle.
 // N'oubliez pas l'héritage !
+
+function Drawing(){
+
+    this.tab= new Array();
+
+
+};
+
+function Rectangle(x,y, longueur, largeur) {
+    this.x =x;
+    this.y=y;
+    this.longueur=longueur;
+    this.largeur=largeur;
+
+};
+
+function Ligne(x1,x2,y1,y2) {
+    this.x1=x1;
+    this.x2=x2;
+    this.y1=y1;
+    this.y2=y2;
+
+
+};
+
+function Forme() {
+
+};
+
