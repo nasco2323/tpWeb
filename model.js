b93429574597c3e2a6aea7efc5b9826be5a926a1
narcissekouadio <kouadio.yao.narcisse2@gmail.com>
2020-01-23 14:12:41 +0100

    creation de la class rectangle  , forme, ligne et drawing

--- model.js
+++ model.js
@@ -1,32 +1,37 @@
-
 // Implémenter ici les 4 classes du modèle.
 // N'oubliez pas l'héritage !
 
-function Drawing(){
+function Drawing() {
 
-    this.tab= new Array();
-
-
-};
-
-function Rectangle(x,y, longueur, largeur) {
-    this.x =x;
-    this.y=y;
-    this.longueur=longueur;
-    this.largeur=largeur;
-
-};
-
-function Ligne(x1,x2,y1,y2) {
-    this.x1=x1;
-    this.x2=x2;
-    this.y1=y1;
-    this.y2=y2;
+    this.tab = new Array();
 
 
 };
 
-function Forme() {
+function Rectangle(x, y, longueur, largeur) {
+    Forme.call(this, x, y, longueur, largeur)
+    this.x = x;
+    this.y = y;
+    this.longueur = longueur;
+    this.largeur = largeur;
+
+};
+Rectangle.prototype= new Forme();
+
+function Ligne(x1, y1, x2, y2) {
+    Forme.call(this,x1,y1,x2,y2)
+    this.x1 = x1;
+    this.x2 = x2;
+    this.y1 = y1;
+    this.y2 = y2;
+
+
+};
+Ligne.prototype= new Forme();
+
+function Forme(couleur , lineborder) {
+    this.couleur= couleur;
+    this.lineborder=lineborder;
 
 };
 
